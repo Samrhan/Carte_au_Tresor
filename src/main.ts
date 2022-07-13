e145c65b7a865f004cd437a437f5b91ddca81a5e
Samrhan <samuel.bader@efrei.net>
2022-07-13 18:53:34 +0200

    typo(app): Fix typo

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,24 @@ import {AppFactory} from "./app/app.factory";
 
 
 async function main() {
+
+    // Getting the program arguments
     const fileName = process.argv.find(arg => arg.startsWith('--file='))?.split('=')[1];
     const outFileName = process.argv.find(arg => arg.startsWith('--output='))?.split('=')[1];
+    const verbose = process.argv.find(arg => arg.startsWith('--verbose'));
+
     if (!fileName) {
         throw new Error('No file specified');
     }
-    const verbose = process.argv.find(arg => arg.startsWith('--verbose'));
     const app = await AppFactory.create(fileName);
+
     if (verbose) {
         app.setVerbose()
     }
     if (outFileName) {
         app.setOutFileName(outFileName)
     }
+
     await app.execute();
 }
 
