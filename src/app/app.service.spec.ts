d70bd680424961bb6f86433b5fda386acf31d3e8
Samrhan <samuel.bader@efrei.net>
2022-07-15 18:35:42 +0200

    test(app): Add some more tests

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -23,32 +23,38 @@ describe(AppService.name, () => {
     });
 
     it("should create a map", async () => {
-
         await appService.execute('test.txt');
         expect(MapFactory.createMap).toHaveBeenCalledWith('');
-
     })
 
     it("should compute movements", async () => {
-
         await appService.execute('test.txt');
         expect(mapService.computeMovements).toHaveBeenCalled();
-
     })
 
     it("should serialize map", async () => {
-
         await appService.execute('test.txt');
         expect(mapService.serialize).toHaveBeenCalledWith();
-
     })
 
     it("should write file", async () => {
-
         await appService.execute('test.txt');
         expect(fs.writeFileSync).toHaveBeenCalledWith('test-result.txt', '');
-
     })
 
+    it("should set verbose to true", async () => {
+        await appService.setVerbose();
+        expect(appService.verbose).toBeTruthy();
+    })
+
+    it("should set out file name", async () => {
+        await appService.setOutFileName('funnyoutfilename.txt');
+        expect(appService.outFileName).toBe('funnyoutfilename.txt');
+    })
+
+    it("should set hardcore mode to true", async () => {
+        await appService.setHardcoreMode();
+        expect(appService.hardcoreMode).toBeTruthy();
+    })
 
 })
